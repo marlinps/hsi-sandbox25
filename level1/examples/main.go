833d17d227a976d5e2076e4f9a8ec1efd5aebd13
marlinps <marlinpurnamas92@gmail.com>
2025-07-31 15:45:23 +0700

    create level1

---
+++ level1/examples/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var usiaSaya int = 40
+	var phi float32 = 3.14
+
+	fmt.Println(usiaSaya)
+	fmt.Println(phi)
+}
+
+/* TODO: standart penulisan di Golang menggunakan camelCase
+TODO: Reminder dalam memilih tipe data jangan asal-asal harus dipertimbangkan sesuai kebutuhan, alokasi memory;server dsb
+*/
