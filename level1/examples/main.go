965fe72e299ce48a9e2650f0d9860bec0f56d0cb
marlinps <marlinpurnamas92@gmail.com>
2025-07-31 16:02:18 +0700

    update level1

--- level1/examples/main.go
+++ level1/examples/main.go
@@ -6,8 +6,12 @@ func main() {
 	var usiaSaya int = 40
 	var phi float32 = 3.14
 
+	var test = "abc"
+	var ApakahSayaBenar bool = true
+
 	fmt.Println(usiaSaya)
 	fmt.Println(phi)
+	fmt.Println(test)
 }
 
 /* TODO: standart penulisan di Golang menggunakan camelCase
