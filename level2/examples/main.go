18fe318ea7c6c2c2a40b8300d726b6989f54ecaa
marlinps <marlinpurnamas92@gmail.com>
2025-08-01 11:44:54 +0700

    update level2(error handling)

--- level2/examples/main.go
+++ level2/examples/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"errors"
+	"fmt"
 )
 
 // TODO: function to divide two numbers with error handling
@@ -14,8 +15,41 @@ func divide(a, b float32) (float32, error) {
 	return a / b, nil
 }
 
-func main() {
+func kalkulasiPembagian(pembilang, pembagi int) (int, int, error) {
+	if pembagi == 0 {
+		return 0, 0, errors.New("cannot divide by zero") // error
+	}
 
+	hasil := pembilang / pembagi
+	sisa := pembilang % pembagi
+
+	return hasil, sisa, nil
+}
+
+func main() {
+	// Exampleof successfull division
+	result, err := divide(10, 2)
+	if err != nil {
+		fmt.Println("Pesan Error: ", err)
+	} else {
+		fmt.Println("Hasil pembagian 10/2 =", result)
+	}
+
+	// Example of division by zero
+	result, err = divide(10, 0)
+	if err != nil {
+		fmt.Println("Error: ", err)
+	} else {
+		fmt.Println("Hasil pembagian 10/0 =", result)
+	}
+
+	// kembalian dengan 3 nilai
+	hasil, sisa, err := kalkulasiPembagian(10, 2)
+	if err != nil {
+		fmt.Println("Pesan Error: ", err)
+	} else {
+		fmt.Println("Hasil pembagian 10/2 =", hasil, "dengan sisa =", sisa)
+	}
 }
 
 /* TODO: Notes
