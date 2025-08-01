1eb3e796531914bf58491d5337d24a1e47c9b600
marlinps <marlinpurnamas92@gmail.com>
2025-08-01 15:06:21 +0700

    update level3(pointer)

--- level3/examples/main.go
+++ level3/examples/main.go
@@ -1,11 +1,28 @@
 package main
 
+import "fmt"
+
 type PesertaSandbox struct {
 	NamaLengkap  string
 	Gender       string
 	Usia         int
 	SudahMenikah bool
 	Domisili     string
+}
+
+// TODO: struct bisa digunakan sebagai argumen di function
+// TODO: akan diteruskan sbg pass by value, mengirimkan dengan nilai sehingga struct dibuat baru atau salin (copy)
+func bertambahUsia(peserta PesertaSandbox) {
+	peserta.Usia += 1
+	fmt.Println("Usian peserta setelah melewati function bertambahUsia =", peserta.Usia)
+}
+
+// TODO: diteruskan sbg pass by reference
+// TODO: struct tidak akan dibuat baru hanya mengirimkan alamat struct yang ada
+
+func bertambahUsiaPointer(peserta *PesertaSandbox) {
+	peserta.Usia += 1
+	fmt.Println("Usian peserta setelah melewati function bertambahUsia =", peserta.Usia)
 }
 
 func main() {
@@ -18,12 +35,28 @@ func main() {
 	pesertaStructVariable.Domisili = "Jakarta"
 
 	// TODO: Inisialisasi struct dengan literal
-	pesertaStructVariable2 := PesertaSandbox{
-		NamaLengkap:  "Ria Rahmawati",
-		Gender:       "Perempuan",
-		Usia:         25,
-		SudahMenikah: false,
-		Domisili:     "Palembang",
-	}
+	// TODO: Umumnya digunakan untuk mendapatkan Default Value dari  sebuah Struct (runtimenya lebih cepat dibandingkan inisialisasi diatas
+
+	// pesertaStructVariable2 := PesertaSandbox{
+	// 	NamaLengkap:  "Ria Rahmawati",
+	// 	Gender:       "Perempuan",
+	// 	Usia:         25,
+	// 	SudahMenikah: false,
+	// 	Domisili:     "Palembang",
+	// }
+
+	// Pass by Value, dimana Struct akan dicopy paste
+	bertambahUsia(pesertaStructVariable) //34
+
+	// Struct aslinya tidak berubah
+	fmt.Println(pesertaStructVariable.Usia) //33
+
+	// TODO: Pointer
+	var alamatAsli string = "Duren Sawit Jakarta Timur"
+	var pointerAlamat *string = &alamatAsli // TODO: Pointer dengan tipe string menunjuk ke memory alamat asli
+
+	fmt.Println("Nilai dari variable alamatAsli :", alamatAsli)
+	fmt.Println("Nilai dari variable Pointer(Memory) alamat asli :", pointerAlamat)
+	fmt.Println("Isi dari variable pointerAlamat :", *pointerAlamat) // TODO: Akan mengembalikan nilai aslinya
 
 }
