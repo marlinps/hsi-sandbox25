ba85483174b9c0a8a3d4a0ed685bfb778ca23c9c
marlinps <marlinpurnamas92@gmail.com>
2025-08-01 08:19:14 +0700

    update level1/examples/main.go

--- level1/examples/main.go
+++ level1/examples/main.go
@@ -4,16 +4,26 @@ import "fmt"
 
 func main() {
 	var usiaSaya int = 40
-	var phi float32 = 3.14
+	// var phi float32 = 3.14
 
-	var test = "abc"
-	var ApakahSayaBenar bool = true
+	// var test = "abc"
+	// var apakahSayaBenar bool = true
 
-	fmt.Println(usiaSaya)
-	fmt.Println(phi)
-	fmt.Println(test)
+	if usiaSaya < 5 {
+		fmt.Println("Ini balita")
+	} else {
+		fmt.Println("Ini bukan lagi balita")
+	}
+
+	//perulangan
+	for i := 1; i < 10; i++ {
+		fmt.Println("Perulangan ke-", i)
+	}
 }
 
 /* TODO: standart penulisan di Golang menggunakan camelCase
 TODO: Reminder dalam memilih tipe data jangan asal-asal harus dipertimbangkan sesuai kebutuhan, alokasi memory;server dsb
+TODO: Flow control di Golang
+1. Validasi (if, switch)
+2. Perulangan (hanya for)
 */
