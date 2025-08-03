0fc14f77fb24b2e77c40eda22008e9892fa68a66
Marlin Purnama Sari <marlinpurnamas92@gmail.com>
2025-08-03 20:22:03 +0700

    create array.go

---
+++ array/main.go
@@ -0,0 +1,16 @@
+package main
+
+func main() {
+	// Declare an array of integers with a fixed size
+	var arrayAngka [10]int // index 0 to 9
+
+	// Assign values to the array
+	arrayAngka[0] = 1
+	arrayAngka[1] = 2
+	arrayAngka[2] = 3
+	arrayAngka[3] = 4
+	arrayAngka[4] = 5
+
+	// Initialize an array of integers
+	arrayAngka1 := [5]int{10, 20, 30, 40, 50}
+}
