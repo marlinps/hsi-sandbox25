8b20b86dc8f7d639ced871c364da7a8bf15bcd69
Marlin Purnama Sari <marlinpurnamas92@gmail.com>
2025-08-10 21:02:47 +0700

    update channel

--- concurrency/main.go
+++ concurrency/main.go
@@ -5,21 +5,26 @@ import (
 	"time"
 )
 
-func cetakPesan(pesanan string) {
+func cetakPesan(pesanan string, channelGoRoutine chan bool) {
 	for i := 0; i < 5; i++ {
 		fmt.Println("Pesanan ke-", i+1, ":", pesanan)
 		time.Sleep(1 * time.Millisecond)
 	}
+
+	channelGoRoutine <- true // mengirim signal bahwa goroutine telah selesai dengan nilai true
 }
 
 func main() {
 	fmt.Println("Memulai pesanan...")
 
+	// inisialisasi channel
+	channel := make(chan bool, 2) // alokasi memory (chan) dan tipe datanya (string, bool atu dll)
+
 	// goROutine Utama
-	go cetakPesan("Nasi Goreng")
+	go cetakPesan("Nasi Goreng", channel)
 
 	//goROutine kedua
-	go cetakPesan("Mie Goreng")
+	go cetakPesan("Mie Goreng", channel)
 
 	// Menunggu goROutine selesai
 	// Dengan cara menunggu beberapa milidetik
