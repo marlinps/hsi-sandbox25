f1a1696311187ceb40adf1b069be033b44986add
Marlin Purnama Sari <marlinpurnamas92@gmail.com>
2025-08-24 17:29:09 +0700

    update  mod-package(import third-party package)

--- praktikum-module/main.go
+++ praktikum-module/main.go
@@ -9,12 +9,17 @@ package main
 import (
 	"fmt"
 	"praktikum-module/calculator"
+
+	// TODO: go get rsc.io/quote
+	"rsc.io/quote" // TODO: Import package dari luar (third-party package), package untuk mendapatkan quote lucu
 )
 
 func main() {
 	hasilTambah := calculator.TambahAngka(5, 3)
 	fmt.Println("Hasil Penjumlahan:", hasilTambah)
-
 	hasilKurang := calculator.KurangAngka(5, 3)
 	fmt.Println("Hasil Pengurangan:", hasilKurang)
+
+	pesan := quote.Hello()
+	fmt.Println("Pesan dari quote package:", pesan)
 }
