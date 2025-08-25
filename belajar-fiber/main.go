f3150914d80bb71d93740ecd8b5ac590c08e6f0b
marlinps <marlinpurnamas92@gmail.com>
2025-08-25 11:52:38 +0700

    create belajar-fiber

---
+++ belajar-fiber/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	// TODO: inisialisasi framework Fiber
+	app := fiber.New()
+
+	// TODO: routing
+	// URL http://localhost:3000/api/
+	// Return "Ahlan Wa Sahlan"
+	app.Get("/api/", func(c *fiber.Ctx) error {
+		return c.SendString("Ahlan Wa Sahlan")
+	})
+
+	app.Listen(":3000")
+}
