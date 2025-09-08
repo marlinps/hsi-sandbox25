11c594e2a3ed8797880ada778b00cc607e4e9284
marlinps <marlinpurnamas92@gmail.com>
2025-09-08 15:14:21 +0700

    update learning gorm

--- gorm/main.go
+++ gorm/main.go
@@ -34,9 +34,9 @@ func main() {
 	// TODO: 4. Insert Data
 	// fmt.Println("Insert Data Suhu")
 	// newData := UkurSuhu{
-	// 	SuhuCelcius:    100,
-	// 	SuhuFahrenheit: 212,
-	// 	SuhuReamur:     80,
+	// 	SuhuCelcius:    130,
+	// 	SuhuFahrenheit: 175,
+	// 	SuhuReamur:     70,
 	// }
 
 	// operasiTambahData := koneksiDb.Create(&newData)
@@ -46,13 +46,30 @@ func main() {
 	// fmt.Println("Data berhasil ditambahkan dengan ID:", newData.ID)
 
 	// TODO: 5.Read Data
-	fmt.Println("\nRead Data Suhu")
-	// Mengggunakakn fungsi First untuk mengambil data pertama
-	var dataSuhu UkurSuhu
-	operasiBacaData := koneksiDb.First(&dataSuhu)
-	if operasiBacaData.Error != nil {
-		log.Fatal("Gagal membaca data:", operasiBacaData.Error.Error())
-	}
-	fmt.Println("Data berhasil dibaca:", dataSuhu)
+	// fmt.Println("\nRead Data Suhu")
+	// // Mengggunakakn fungsi First untuk mengambil data pertama
+	// var readData UkurSuhu
+	// operasiBacaData := koneksiDb.First(&readData)
+	// if operasiBacaData.Error != nil {
+	// 	log.Fatal("Gagal membaca data:", operasiBacaData.Error.Error())
+	// }
+	// fmt.Println("Data berhasil dibaca:", readData)
+
+	// TODO: 6. Update Data
+	// fmt.Println("\nUpdate Data Suhu")
+	// var updateData UkurSuhu
+	// koneksiDb.First(&updateData, 1) // Mengambil data pertama untuk diupdate
+	// updateData.SuhuCelcius = 50
+	// updateData.SuhuFahrenheit = 122
+	// updateData.SuhuReamur = 40
+	// koneksiDb.Save(&updateData)
+	// fmt.Println("Data berhasil diupdate:", updateData)
+
+	// TODO: 7. Delete Data
+	fmt.Println("\nDelete Data Suhu")
+	var deleteDataId UkurSuhu
+	koneksiDb.First(&deleteDataId, 4) // Mengambil data pertama untuk dihapus, ID 4
+	koneksiDb.Delete(&deleteDataId)
+	fmt.Println("Data berhasil dihapus:", deleteDataId)
 
 }
