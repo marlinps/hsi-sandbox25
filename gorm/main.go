2486f32a2fbe572652151ffa36b31889c80477cd
marlinps <marlinpurnamas92@gmail.com>
2025-09-08 14:55:25 +0700

    update learning gorm

--- gorm/main.go
+++ gorm/main.go
@@ -2,13 +2,14 @@ package main
 
 // TODO: other imports
 import (
+	"fmt"
 	"log"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
-// TODO: definisi Model/Struktur Tabel
+// TODO: definisi Model/Struktur Tabel menjadi tabel ukur_suhus(default penamaan)
 type UkurSuhu struct {
 	ID             uint `gorm:"primaryKey"` // TODO: sesuaikan tipe data dan tag GORM
 	SuhuCelcius    float64
@@ -29,5 +30,29 @@ func main() {
 
 	// TODO: 3. Automigrasi (Migrasi struktur tabel dari struct)
 	koneksiDb.AutoMigrate(&UkurSuhu{})
+
+	// TODO: 4. Insert Data
+	// fmt.Println("Insert Data Suhu")
+	// newData := UkurSuhu{
+	// 	SuhuCelcius:    100,
+	// 	SuhuFahrenheit: 212,
+	// 	SuhuReamur:     80,
+	// }
+
+	// operasiTambahData := koneksiDb.Create(&newData)
+	// if operasiTambahData.Error != nil {
+	// 	log.Fatal("Gagal menambahkan data:", operasiTambahData.Error.Error())
+	// }
+	// fmt.Println("Data berhasil ditambahkan dengan ID:", newData.ID)
+
+	// TODO: 5.Read Data
+	fmt.Println("\nRead Data Suhu")
+	// Mengggunakakn fungsi First untuk mengambil data pertama
+	var dataSuhu UkurSuhu
+	operasiBacaData := koneksiDb.First(&dataSuhu)
+	if operasiBacaData.Error != nil {
+		log.Fatal("Gagal membaca data:", operasiBacaData.Error.Error())
+	}
+	fmt.Println("Data berhasil dibaca:", dataSuhu)
 
 }
